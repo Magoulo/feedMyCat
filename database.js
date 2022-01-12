069137a4478b6f07c63362c888c546200efee71f
Magoulo <83534780+Magoulo@users.noreply.github.com>
2022-01-12 12:40:56 +0100

    fixat databas filen

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const sqlite = require('sqlite3')
 const db = new sqlite.Database('catDB.db')
 
 db.run(`
-   CREATE TABLE IF NOT EXISTS "cat_owner" (
+CREATE TABLE IF NOT EXISTS "cat_owner" (
 	"Cat_owner_id"	INTEGER,
 	"Firstname"	TEXT,
 	"Lastname"	TEXT,
@@ -61,7 +61,6 @@ exports.getAllFaq = function (callback) {
         callback(error, faq)
     })
 }
-
 exports.createFaq = function (Question, Answer, callback) {
 
     const query = "INSERT INTO faq (Question, Answer) VALUES(?,?)"
@@ -72,14 +71,14 @@ exports.createFaq = function (Question, Answer, callback) {
         callback(error, this.lastID)
     })
 }
-
 exports.getFaqById = function (Faq_id, callback) {
 
     const query = "SELECT * FROM faq WHERE Faq_id = ? LIMIT 1"
     const values = [Faq_id]
 
     db.get(query, values, function (error, faq) {
-        if(faq == undefined){
+        callback(error, faq)
+       /* if(faq == undefined){
             model = {
                 error,
                 faq
@@ -88,47 +87,35 @@ exports.getFaqById = function (Faq_id, callback) {
         }
         else{
              callback(error, faq)
-        }
+        }*/
     })
 }
-
 exports.updateFaqById = function (Faq_id, Question, Answer, callback) {
-
     const query = "UPDATE faq SET Question = ?, Answer = ? WHERE Faq_id = ?"
     const values = [Question, Answer, Faq_id]
 
     db.run(query, values, function (error) {
-
         callback(error)
-
     })
 }
-
 exports.deleteFaqById = function (Faq_id, callback) {
-
     const query = "Delete FROM faq WHERE Faq_id = ? "
     const values = [Faq_id]
 
     db.run(query, values, function (error) {
-
         callback(error, this.lastID)
-
     })
 }
 
-//--------------------------------------------Donation-----------------------------------------------------------------
-
+//--------------------------------------------Donation alternatives-----------------------------------------------------------------
 exports.getAllDon = function (callback) {
-
     const query = "SELECT * FROM donation_alternative"
 
     db.all(query, function (error, donation_alternative) {
         callback(error, donation_alternative)
     })
 }
-
 exports.createDon = function (Name, Description, Website_link, callback) {
-
     const query = "INSERT INTO donation_alternative (Name, Description, Website_link) VALUES(?,?,?)"
     const values = [Name, Description, Website_link]
 
@@ -137,14 +124,13 @@ exports.createDon = function (Name, Description, Website_link, callback) {
         callback(error, this.lastID)
     })
 }
-
 exports.getDonById = function (Donation_alternative_id, callback) {
-
     const query = "SELECT * FROM donation_alternative WHERE Donation_alternative_id = ? LIMIT 1"
     const values = [Donation_alternative_id]
 
     db.get(query, values, function (error, donation_alternative) {
-        if(donation_alternative == undefined){
+        callback(error, donation_alternative)
+       /* if(donation_alternative == undefined){
             model = {
                 error,
                 donation_alternative
@@ -153,12 +139,10 @@ exports.getDonById = function (Donation_alternative_id, callback) {
         }
         else{
              callback(error, donation_alternative)
-        }
+        }*/
     })
 }
-
 exports.updateDonById = function (Donation_alternative_id, Name, Description, Website_link, callback) {
-
     const query = "UPDATE donation_alternative SET Name = ?, Description = ?, Website_link = ? WHERE Donation_alternative_id = ?"
     const values = [Name, Description, Website_link, Donation_alternative_id]
 
@@ -167,49 +151,40 @@ exports.updateDonById = function (Donation_alternative_id, Name, Description, We
 
     })
 }
-
 exports.deleteDonById = function (Donation_alternative_id, callback) {
-
     const query = "Delete FROM donation_alternative WHERE Donation_alternative_id = ? "
     const values = [Donation_alternative_id]
 
     db.run(query, values, function (error) {
-
         callback(error)
-
     })
 }
 
-//-----------------------------------Index----------------------------------------------------------------------
+//-----------------------------------Cat------------------------------------------------
 
 exports.getAllCat = function (startIndex,endIndex,callback) {
-
     const query = "SELECT * FROM cat JOIN cat_owner ON cat.Cat_owner_id = cat_owner.Cat_owner_id ORDER BY Cat_id DESC LIMIT ?,?"
     const values = [startIndex,endIndex]
+
     db.all(query,values, function (error, cat) {
         callback(error, cat)
     })
 }
-
 exports.createCat = function (Name, Description, Image_reference, Cat_owner_Id, Age, callback) {
-
     const query = "INSERT INTO cat (Name,Description,Image_reference,Cat_owner_Id, Age) VALUES(?,?,?,?,?)"
     const values = [Name, Description, Image_reference, Cat_owner_Id, Age]
 
     db.run(query, values, function (error) {
-
         callback(error, this.lastID)
     })
 }
-
 exports.getCatById = function (Cat_id, callback) {
-
     const query = "SELECT * FROM cat WHERE Cat_id = ? LIMIT 1"
     const values = [Cat_id]
 
-    
     db.get(query, values, function (error, cat) {
-        if(cat == undefined){
+        callback(error, cat)
+       /* if(cat == undefined){
             model = {
                 error,
                 cat
@@ -217,13 +192,11 @@ exports.getCatById = function (Cat_id, callback) {
             callback(model)
         }
         else{
-             callback(error, cat)
-        }
+            callback(error, cat)
+        }*/
     })
 }
-
 exports.updateCatById = function (Cat_id, Name, Description, Age, callback) {
-
     const query = "UPDATE cat SET Name = ?, Description = ?, Age = ? WHERE Cat_id = ?"
     const values = [Name, Description, Age, Cat_id]
 
@@ -231,28 +204,23 @@ exports.updateCatById = function (Cat_id, Name, Description, Age, callback) {
         callback(error)
     })
 }
-
 exports.deleteCatById = function (Cat_id, callback) {
-
     const query = "Delete FROM cat WHERE Cat_id = ? "
     const values = [Cat_id]
 
     db.run(query, values, function (error) {
-
         callback(error)
-
     })
 }
 
-
 //-----------------------------------------Catownerinfo------------------------------------------------
-
-exports.getCatownerinfoById = function (id, callback) {
+exports.getCatownerinfoById = function (Id, callback) {
     const query = "SELECT * FROM cat_owner WHERE Cat_owner_id = ? LIMIT 1"
-    const values = [id]
+    const values = [Id]
 
     db.get(query, values, function (error, cat_owner) {
-      if(cat_owner == undefined){
+        callback(error, cat_owner)
+      /*  if(cat_owner == undefined){
           model = {
               error,
               cat_owner
@@ -261,50 +229,41 @@ exports.getCatownerinfoById = function (id, callback) {
       }
       else{
            callback(error, cat_owner)
-      } 
+      } */
 })
 }
-
 exports.getAllCatownerinfo = function (callback) {
-
     const query = "SELECT * FROM cat_owner"
 
     db.all(query, function (error, catownerinfo) {
         callback(error, catownerinfo)
     })
 }
-
 exports.createCatownerinfo = function (Firstname, Lastname, Email, Phonenumber, City, callback) {
-
     const query = "INSERT INTO cat_owner (Firstname,Lastname,Email,Phonenumber,City) VALUES(?,?,?,?,?)"
     const values = [Firstname, Lastname, Email, Phonenumber, City]
 
     db.run(query, values, function (error) {
         callback(error, this.lastID)
     })
-
 }
 exports.deleteCatownerById = function (Cat_owner_id, callback) {
-
     const query = "Delete FROM cat_owner WHERE Cat_owner_id = ? "
     const values = [Cat_owner_id]
 
     db.run(query, values, function (error) {
-
         callback(error)
-
     })
 }
 
-//---------------------------------Account----------------------------------------------------------------
-
+//---------------------------------User accounts----------------------------------------------------------------
 exports.getUseraccount = function (Username, callback) {
-
     const query = "SELECT * FROM user_accounts WHERE Username = ? LIMIT 1"
     const values = [Username]
 
     db.get(query, values, function (error, User_accounts) {
-        if(User_accounts == undefined){
+        callback(error, User_accounts)
+       /* if(User_accounts == undefined){
             model = {
                 error,
                 User_accounts
@@ -312,26 +271,22 @@ exports.getUseraccount = function (Username, callback) {
             callback(model)
         }
         else{
-             callback(error, User_accounts)
-        } 
+            callback(error, User_accounts)
+        } */
     })
 }
-
 exports.createUseraccount = function (Username, Password, Cat_owner_id, callback) {
-
     const query = "INSERT INTO user_accounts (Username,Password,Cat_owner_id) VALUES(?,?,?)"
     const values = [Username, Password, Cat_owner_id]
 
     db.run(query, values, function (error) {
-
         callback(error, this.lastID)
     })
 }
-
 exports.getMyads = function (user_id, callback) {
-
     const query = "SELECT * FROM cat JOIN cat_owner ON cat.Cat_owner_id = cat_owner.Cat_owner_id JOIN user_accounts ON cat_owner.cat_owner_id = user_accounts.cat_owner_id WHERE User_accounts.Account_id = ? ORDER BY Cat_id DESC"
-    values = [user_id]
+    const values = [user_id]
+    
     db.all(query, values, function (error, cat) {
         callback(error, cat)
     })
